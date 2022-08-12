d52118ea2db786d009227e8f953f0f35bee61eeb
Asmaa_Masri <asmaamasri1997@gmail.com>
2022-08-12 20:34:01 +0300

    last commit

--- .history/server_20220812201720.js
+++ .history/server_20220812201720.js
@@ -1,6 +1,11 @@
 const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+/*app.set("views", "Views");
+app.use(express.static("Views"));
+/*app.get("/", (req, res) => {
+  return res.render("index.html");
+});*/
 app.get("/", (req, res) => res.sendFile(__dirname + "/Views/index.html"));
 
 io.on("connection", (socket) => {
